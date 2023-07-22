dcc65ecc00ce3ab1dec87da78babe770c2774798
wooyeon <bethelight730@gmail.com>
2023-07-22 17:50:45 +0900

    일시정지 해야됨

---
+++ Maple's Flight/Assets/Scripts/PauseController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public GameObject PauseUI;
+    public AudioSource click;
+
+    private bool paused = false;
+
+    public void Pause() // 일시정지
+    {
+        paused = true;
+        GameManager.instance.isPlaying = false;
+    }
+
+    public void Resume() // 재개
+    {
+        paused = false;
+        GameManager.instance.isPlaying = true;
+    }
+
+    public void StopGame() // 게임 나가기
+    {
+        paused = false;
+    }
+
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if (!paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
