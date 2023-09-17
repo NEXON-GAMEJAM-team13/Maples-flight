7ca132443646b3ac32bc607bb3d9ecc2dec3f09b
wooyn730 <80298207+wooyn730@users.noreply.github.com>
2023-09-17 21:07:19 +0900

    Update GameOver.cs

--- Maple's Flight/Assets/Scripts/GameOver.cs
+++ Maple's Flight/Assets/Scripts/GameOver.cs
@@ -16,37 +16,20 @@ public class GameOver : MonoBehaviour
     [Header("=그 외=")]
     [SerializeField]
     GameObject retryBtn;
-    [SerializeField]
-    GameObject newScore;
-    [SerializeField]
-    GameObject newEnd;
 
     private void OnEnable()
     {
+        GameManager.instance.Save();
+
         thumbnail.sprite = GameManager.instance.EndingController.GetEndingImage(GameManager.instance.endingNow);
         titleTxt.text = GameManager.instance.EndingController.GetEndingTitleText(GameManager.instance.endingNow);
         descTxt.text = GameManager.instance.EndingController.GetEndingDescText(GameManager.instance.endingNow);
 
         Invoke("RetryAble", 1f);
-
-        CheckNew();
-
-        GameManager.instance.Save();
     }
 
     void RetryAble()
     {
         retryBtn.GetComponent<Button>().interactable = true;
-    }
-
-    void CheckNew()
-    {
-        if (GameManager.instance.isNewScore)
-            newScore.SetActive(true);
-        if (GameManager.instance.isNewEnd)
-            newEnd.SetActive(true);
-
-        Debug.Log(GameManager.instance.isNewScore);
-        Debug.Log(GameManager.instance.isNewEnd);
     }
 }
